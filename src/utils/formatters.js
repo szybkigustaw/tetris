c01712b5d06aaee248b04d8dd092080a3482a0c0
Polaczeq22 <mikulamichal@pm.me>
2023-02-21 11:14:36 +0100

    Added hour display

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -2,5 +2,8 @@ export const transformTime = function(seconds){
 		let minutes = parseInt(seconds / 60);
 		let seconds_left = seconds - (minutes * 60);
 
-		return minutes > 0 ? `${minutes}m ${seconds_left}s` : `${seconds_left}s`;
+        let hours = parseInt(minutes / 60);
+        let minutes_left = minutes - (hours * 60);
+
+		return (hours > 0 ? `${hours}g ${minutes_left}m ${seconds_left}s` : (minutes > 0 ? `${minutes}m ${seconds_left}s` : `${seconds_left}s`));
 }
